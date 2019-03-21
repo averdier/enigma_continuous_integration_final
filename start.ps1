964816edc25869d9717c887e0b49b530f68d76c7
Arthur <a.verdier@outlook.fr>
2019-03-21 12:07:22 +0100

    Update ps1

--- start.ps1
+++ start.ps1
@@ -1,10 +1,11 @@
 git clone -b v1 https://github.com/Akasam/samplephpwebsite
 
-docker run -v ${PWD}:/app --rm phpunit/phpunit:latest tests/TestFunctions.php
+docker container create --name temp_v1 -v samplephpwebsite_site_v1:/site -v samplephpwebsite_conf_v1:/conf -v samplephpwebsite_bdd_install_v3:/bdd/install hello-world
+docker cp ${PWD}/samplephpwebsite/. temp_v1:/site/src
+docker cp ${PWD}/tests/. temp_v1:/site/tests/
+docker cp ${pwd}/nginx/. temp_v1:/conf
+docker rm temp_v1
 
-docker container create --name temp -v samplephpwebsite_site:/site -v samplephpwebsite_conf:/conf hello-world
-docker cp ./samplephpwebsite/. temp:/site/html
-docker cp ./nginx/. temp:/conf
-docker rm temp
+docker run -v samplephpwebsite_site_v1:/app --rm phpunit/phpunit:latest tests/TestFunctions.php
 
 docker-compose up -d
