61031a0824b1bd49c001584a843a6e0680107118
Arthur <a.verdier@outlook.fr>
2019-03-21 10:51:19 +0100

    Update Groovy

--- jobs/build.groovy
+++ jobs/build.groovy
@@ -12,7 +12,7 @@ job('Sample PHP website V1 Job') {
         shell("git clone -b v1 https://github.com/Akasam/samplephpwebsite")
         shell("docker container create --name temp_groovy -v samplephpwebsite_site:/site -v samplephpwebsite_conf:/conf hello-world")
         shell('docker cp "$(pwd)/samplephpwebsite/." temp_groovy:/site')
-        shell('docker cp "$(pwd)/tests/." temp_groovy:/site')
+        shell('docker cp "$(pwd)/tests/." temp_groovy:/site/tests/')
         shell("docker cp ./nginx/. temp_groovy:/conf")
         shell("docker rm temp_groovy")
         shell('docker run -v samplephpwebsite_site:/app --rm phpunit/phpunit:latest tests/TestFunctions.php')
