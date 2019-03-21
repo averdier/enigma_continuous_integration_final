a45f3bfbc16e3eb45592a46df40b0cd6379e332b
Arthur <a.verdier@outlook.fr>
2019-03-21 14:59:40 +0100

    Update

---
+++ README.md
@@ -0,0 +1,31 @@
+# Sample PHP Website V1
+
+Dépendances:
+- Git
+- Docker
+- Docker compose (version >=3)
+
+## Installation
+
+Cloner le projet
+```
+git clone https://github.com/averdier/enigma_continuous_integration_final
+```
+
+## Lancer le projet
+
+Windows
+```
+.\start.ps1
+```
+
+Linux / MacOS (non testé)
+```
+chmod +x start.sh
+
+./start.sh
+```
+
+## Jenkins
+
+Les scripts sont disponibles dans le dossier `jobs`
---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone -b v2 https://github.com/Akasam/samplephpwebsite
+
+docker container create --name temp_v2 -v samplephpwebsite_site_v2:/site -v samplephpwebsite_conf_v2:/conf hello-world
+docker cp "$(pwd)/samplephpwebsite/." temp_v2:/site/src
+docker cp "$(pwd)/tests/." temp_v2:/site/tests/
+docker cp "$(pwd)/nginx/." temp_v2:/conf
+docker rm temp_v2
+
+docker run -v samplephpwebsite_site_v2:/app --rm phpunit/phpunit:latest tests/TestFunctions.php
+
+docker-compose up -d
