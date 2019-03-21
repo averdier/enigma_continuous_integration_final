95a44047c9f19ba02ea7cdb328e47f2b7a86dbbc
Arthur <a.verdier@outlook.fr>
2019-03-21 11:39:23 +0100

    Update groovy script

--- jobs/build.groovy
+++ jobs/build.groovy
@@ -10,12 +10,12 @@ job('Sample PHP website V2 Job') {
     }
     steps {
         shell("git clone -b v2 https://github.com/Akasam/samplephpwebsite")
-        shell("docker container create --name temp_groovy -v samplephpwebsite_site:/site -v samplephpwebsite_conf:/conf hello-world")
-        shell('docker cp "$(pwd)/samplephpwebsite/." temp_groovy:/site/src')
-        shell('docker cp "$(pwd)/tests/." temp_groovy:/site/tests/')
-        shell('docker cp "$(pwd)/nginx/." temp_groovy:/conf')
-        shell("docker rm temp_groovy")
+        shell("docker container create --name temp_groovy_v2 -v samplephpwebsite_site_v2:/site -v samplephpwebsite_conf_v2:/conf hello-world")
+        shell('docker cp "$(pwd)/samplephpwebsite/." temp_groovy_v2:/site/src')
+        shell('docker cp "$(pwd)/tests/." temp_groovy_v2:/site/tests/')
+        shell('docker cp "$(pwd)/nginx/." temp_groovy_v2:/conf')
+        shell("docker rm temp_groovy_v2")
         shell("rm -r samplephpwebsite")
-        shell('docker run -v samplephpwebsite_site:/app --rm phpunit/phpunit:latest tests/TestFunctions.php')
+        shell('docker run -v samplephpwebsite_site_v2:/app --rm phpunit/phpunit:latest tests/TestFunctions.php')
     }
 }
